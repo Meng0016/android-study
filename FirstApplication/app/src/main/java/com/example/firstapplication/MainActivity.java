9a27802d014b3617c526bb336eea4c287748f1e7
Meng <103471013+Meng0016@users.noreply.github.com>
2022-08-16 15:24:14 +0800

    first day

---
+++ FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        TextView tv_one = findViewById(R.id.tv_one);
+//        tv_one.setText("Meng");
+
+    }
+}
