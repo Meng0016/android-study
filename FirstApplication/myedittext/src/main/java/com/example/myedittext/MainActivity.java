934da7ddbff0065505202a5cd122ac114848f592
Meng <103471013+Meng0016@users.noreply.github.com>
2022-08-17 22:23:30 +0800

    second day

---
+++ FirstApplication/myedittext/src/main/java/com/example/myedittext/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myedittext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                Log.e("meng", "onClick: "+text );
+                editText.setText(text + "!");
+                System.out.println("onClick: "+text );
+            }
+        });
+    }
+}
